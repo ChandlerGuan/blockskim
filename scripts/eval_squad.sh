3ce9634aea0abd3e188289117804b14ea81e04f7
ChandlerGuan <bonboru.guan@gmail.com>
2021-07-28 14:59:13 +0800

    debug: stash files for debug

---
+++ scripts/eval_squad.sh
@@ -0,0 +1,47 @@
+OUTPUT_DIR=model/tmp/eval/debug
+DATA_DIR=datasets/squad
+
+if [ -d "$OUTPUT_DIR" ]; then
+  OUTPUT_DIR=${OUTPUT_DIR}_$(date +"%m-%d-%H-%M")
+fi
+
+mkdir -p ${OUTPUT_DIR}
+
+# python src/run_qa_no_trainer.py \
+#   --model_name_or_path bert-base-uncased \
+#   --dataset_name squad \
+#   --per_device_train_batch_size 12 \
+#   --learning_rate 3e-5 \
+#   --num_train_epochs 2 \
+#   --max_seq_length 512 \
+#   --doc_stride 128 \
+#   --pad_to_max_length \
+#   --output_dir ${OUTPUT_DIR} 2>&1 | tee ${OUTPUT_DIR}/log_finetune.log
+
+# python src/run_qa.py \
+#   --model_name_or_path bert-base-uncased \
+#   --dataset_name squad \
+#   --do_train \
+#   --do_eval \
+#   --per_device_train_batch_size 12 \
+#   --learning_rate 3e-5 \
+#   --num_train_epochs 2 \
+#   --max_seq_length 512 \
+#   --doc_stride 128 \
+#   --overwrite_output_dir \
+#   --output_dir ${OUTPUT_DIR} 2>&1 | tee ${OUTPUT_DIR}/log_finetune.log
+
+python src/run_squad.py \
+  --model_type bert \
+  --block_skim \
+  --model_name_or_path bert-base-uncased \
+  --do_lower_case \
+  --do_eval \
+  --predict_file dev-v1.1.json \
+  --data_dir ${DATA_DIR} \
+  --per_gpu_eval_batch_size=16 \
+  --max_seq_length 512 \
+  --doc_stride 128 \
+  --overwrite_output_dir \
+  --overwrite_cache \
+  --output_dir ${OUTPUT_DIR} 2>&1 | tee ${OUTPUT_DIR}/log_finetune.log
---
+++ src/configuration_bert_skim.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" BERT model configuration """
+from collections import OrderedDict
+from typing import Mapping
+
+from transformers.configuration_utils import PretrainedConfig
+
+class BertWithSkimConfig(PretrainedConfig):
+    model_type = "bert-skim"
+
+    def __init__(
+        self,
+        vocab_size=30522,
+        hidden_size=768,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.1,
+        attention_probs_dropout_prob=0.1,
+        max_position_embeddings=512,
+        type_vocab_size=2,
+        initializer_range=0.02,
+        layer_norm_eps=1e-12,
+        pad_token_id=0,
+        gradient_checkpointing=False,
+        position_embedding_type="absolute",
+        use_cache=True,
+        block_size=32,
+        max_seq_length=512,
+        **kwargs
+    ):
+        super().__init__(pad_token_id=pad_token_id, **kwargs)
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
+        self.gradient_checkpointing = gradient_checkpointing
+        self.position_embedding_type = position_embedding_type
+        self.use_cache = use_cache
+        self.block_size = block_size
+        self.max_seq_length = max_seq_length
