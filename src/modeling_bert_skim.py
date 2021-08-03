f1470ca293647c5feb786767d6bf6d182a6d1f76
ChandlerGuan <bonboru.guan@gmail.com>
2021-08-03 13:55:44 +0800

    Update: actual skim in bert encoder

--- src/modeling_bert_skim.py
+++ src/modeling_bert_skim.py
@@ -192,6 +192,70 @@ class QuestionAnsweringModelOutputWithSkim(QuestionAnsweringModelOutput):
     attentions: Optional[Tuple[torch.FloatTensor]] = None
     all_skim_mask: Optional[Tuple[torch.FloatTensor]] = None
 
+def skim_hidden_state_tensor(hidden_states, skim_mask_prediction, block_size):
+    """
+    performance input sequence dim reduction on hidden states tensor
+    hidden_states: [batch_size, input_len, hidden_size]
+    skim_mask: [batch_size, index]
+    """
+    hidden_states = hidden_states.view(hidden_states.shape[0], -1, self.block_size, hidden_states.shape[2])
+    hidden_states = hidden_states.gather(dim=1, index=skim_mask_prediction.view(*skim_mask_prediction.shape,1,1).expand(-1,-1,block_size,hidden_states.shape[-1]))
+    hidden_states = hidden_states.view(hidden_states.shape[0],-1,hidden_states.shape[-1])
+    return hidden_states
+
+def skim_attention_mask_tensor(attention_mask, skim_mask_prediction):
+    origin_shape = attention_mask.shape
+    attention_mask = attention_mask.view(*attention_mask.shape[:3], -1, self.block_size)
+    attention_mask = attention_mask.gather(dim=3, index=skim_mask_prediction.view(skim_mask_prediction.shape[0],1,1,skim_mask_prediction.shape[1],1).expand(-1,attention_mask.shape[1],attention_mask.shape[2],-1,self.block_size))
+    attention_mask = attention_mask.view(*origin_shape)
+
+def compute_skim_prediction_aligned(skim_mask, block_size):
+    """
+    compute the skim prediction for a batch of input to align
+    skim_indices [batch, sequence_length]
+        0 for skimming
+        1 for remained
+    """
+    skim_mask = skim_mask.softmax(dim=-1)
+
+    # skim decision with threshold 0.5
+    skim_mask_prediction = skim_mask.argmax(dim=-1)
+
+    # calculate input with most tokens remained
+    max_remained_length = skim_mask_prediction.sum(dim=-1).max()
+
+    # calculate mask with same tokens remained
+    _, skim_indices = skim_mask[:,:,1].topk(dim=1,k=max_remained_length)
+
+    aligned_skim_mask = torch.zeros_like(skim_mask_prediction)
+    aligned_skim_mask = aligned_skim_mask.scatter(dim=1, index=skim_indices, value=1)
+
+    aligned_skim_mask = aligned_skim_mask.unsqueeze(-1).repeat(1,1,block_size).view(aligned_skim_mask.shape[0],-1)
+
+    return aligned_skim_mask
+
+def trunc_with_mask_batched(input, mask, dim):
+    """
+    trunc a batched input at dim
+        e.g. hidden_states ([batch, seq_len, hidden_size])
+            attention_mask ([batch, layer, head, seq_len])
+    mask: [batch, seq_len]
+    """
+    assert input.shape[dim]==mask.shape[1]
+
+    if dim != 1:
+        input = input.transpose(1, dim)
+
+    transpose_shape = list(input.shape)
+    transpose_shape[1] = -1
+
+    trunc_input = input[mask.to(dtype=torch.bool)].view(transpose_shape)
+
+    if dim != 1:
+        trunc_input = trunc_input.transpose(1, dim)
+
+    return trunc_input
+
 class BertEmbeddings(nn.Module):
     """Construct the embeddings from word, position and token_type embeddings."""
 
@@ -568,6 +632,8 @@ class BertEncoder(nn.Module):
         self.config = config
         self.layer = nn.ModuleList([BertLayer(config) for _ in range(config.num_hidden_layers)])
 
+        self.actual_skim = config.actual_skim
+
     def forward(
         self,
         hidden_states,
@@ -636,6 +702,13 @@ class BertEncoder(nn.Module):
                 if self.config.add_cross_attention:
                     all_cross_attentions = all_cross_attentions + (layer_outputs[2],)
             all_skim_mask = all_skim_mask + (layer_outputs[-1],)
+
+            # preform actual skim of input dim reduction with predicted mask
+            if self.actual_skim:
+                skim_mask = layer_outputs[-1]
+                skim_mask_prediction = compute_skim_prediction_aligned(skim_mask, 32)
+                hidden_states = trunc_with_mask_batched(hidden_states, skim_mask_prediction, dim=1)
+                attention_mask = trunc_with_mask_batched(attention_mask, skim_mask_prediction, dim=3)
 
         if output_hidden_states:
             all_hidden_states = all_hidden_states + (hidden_states,)
@@ -1901,6 +1974,8 @@ def test_BertSelfAttention():
 
     config = BertConfig.from_pretrained('bert-base-uncased')
     config.max_seq_length = 512
+    config.block_size = 32
+    config.actual_skim = True
 
     self_attention = BertSelfAttention(config)
 
@@ -1913,8 +1988,8 @@ def test_BertSelfAttention():
 def test_BertForQuestionAnswering():
     config = BertConfig.from_pretrained('bert-base-uncased')
     config.max_seq_length = 512
-
-    print()
+    config.block_size = 32
+    config.actual_skim = True
 
     bert = BertForQuestionAnswering(config)
 
@@ -1924,8 +1999,6 @@ def test_BertForQuestionAnswering():
 
     print(y[0].shape)
 
-    import IPython
-    IPython.embed()
 
 if __name__ == "__main__":
     # test_BertSelfAttention()
