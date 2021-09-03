bd37cf577ca86089f79d7843a4ea8a0c80333c52
ChandlerGuan <bonboru.guan@gmail.com>
2021-09-03 16:04:01 +0800

    Revert "Merge branch 'exp_hotpot' of https://github.com/ChandlerGuan/blockskim into exp_mrqa"
    This reverts commit 936f42cf355ba70b8174476894080df03a0b8767, reversing
    changes made to b5fd6e5ac248ae20be56a65056399307378696bc.

--- src/squad/transformer_squad_processor.py
+++ src/squad/transformer_squad_processor.py
@@ -1,76 +1,6 @@
-from transformers.data.processors.squad import SquadResult, SquadV1Processor, SquadV2Processor, SquadProcessor, _is_whitespace
+from transformers.data.processors.squad import SquadResult, SquadV1Processor, SquadV2Processor, SquadExample, SquadProcessor
 from tqdm import tqdm
 
-class SquadExample:
-    """
-    A single training/test example for the Squad dataset, as loaded from disk.
-
-    Args:
-        qas_id: The example's unique identifier
-        question_text: The question string
-        context_text: The context string
-        answer_text: The answer string
-        start_position_character: The character position of the start of the answer
-        title: The title of the example
-        answers: None by default, this is used during evaluation. Holds answers as well as their start positions.
-        is_impossible: False by default, set to True if the example has no possible answer.
-    """
-
-    def __init__(
-        self,
-        qas_id,
-        question_text,
-        context_text,
-        answer_text,
-        start_position_character,
-        title,
-        answers=[],
-        is_impossible=False,
-        supporting_fact_position=[],
-    ):
-        self.qas_id = qas_id
-        self.question_text = question_text
-        self.context_text = context_text
-        self.answer_text = answer_text
-        self.title = title
-        self.is_impossible = is_impossible
-        self.answers = answers
-
-        self.start_position, self.end_position = 0, 0
-
-        doc_tokens = []
-        char_to_word_offset = []
-        prev_is_whitespace = True
-
-        # Split on whitespace so that different tokens may be attributed to their original position.
-        for c in self.context_text:
-            if _is_whitespace(c):
-                prev_is_whitespace = True
-            else:
-                if prev_is_whitespace:
-                    doc_tokens.append(c)
-                else:
-                    doc_tokens[-1] += c
-                prev_is_whitespace = False
-            char_to_word_offset.append(len(doc_tokens) - 1)
-
-        self.doc_tokens = doc_tokens
-        self.char_to_word_offset = char_to_word_offset
-
-        # Start and end positions only has a value during evaluation.
-        if start_position_character is not None and not is_impossible:
-            self.start_position = char_to_word_offset[start_position_character]
-            self.end_position = char_to_word_offset[
-                min(start_position_character + len(answer_text) - 1, len(char_to_word_offset) - 1)
-            ]
-
-        self.supporting_fact_position = []
-        for fact in supporting_fact_position:
-            fact_start_position = char_to_word_offset[fact[0]]
-            fact_end_position = char_to_word_offset[
-                min(fact[0] + len(fact[1]) - 1, len(char_to_word_offset) - 1)
-            ]
-            self.supporting_fact_position.append((fact_start_position, fact_end_position))
 class SquadProcessorForMask(SquadProcessor):
     def _create_examples(self, input_data, set_type):
         is_training = set_type == "train"
@@ -94,8 +24,6 @@ class SquadProcessorForMask(SquadProcessor):
                         start_position_character = answer["answer_start"]
                         answers = qa["answers"]
 
-                    supporting_facts = [(fact["fact_start"], fact["text"]) for fact in  qa["supporting_facts"]]
-
                     example = SquadExample(
                         qas_id=qas_id,
                         question_text=question_text,
@@ -105,7 +33,6 @@ class SquadProcessorForMask(SquadProcessor):
                         title=title,
                         is_impossible=is_impossible,
                         answers=answers,
-                        supporting_fact_position=supporting_facts
                     )
                     examples.append(example)
         return examples
