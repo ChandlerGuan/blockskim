4e9df390e003650d0bfe0e3d0359d77ce8ecb1cc
ChandlerGuan <bonboru.guan@gmail.com>
2021-09-07 23:36:37 +0800

    update: hotpot exp

--- scripts/profile_flops.sh
+++ scripts/profile_flops.sh
@@ -1,9 +1,10 @@
 OUTPUT_DIR=model/tmp/eval/prof
-DATA_DIR=/home/yguan/blockskim/datasets/squad
+# DATA_DIR=/home/yguan/blockskim/datasets/squad
+DATA_DIR=/home/yguan/blockskim/datasets/hotpotqa
 
 # EVAL_CKPT_DIR=model/block_skim/bert_large_wwm_new/skim_1_balance_20_seed_43/
 # EVAL_CKPT_DIR=model/block_skim/skim_0.1_balance_20/
-EVAL_CKPT_DIR=model/block_skim/albert_base/skim_0.1_balance_20_09-03-16-27/
+EVAL_CKPT_DIR=model/hotpotqa/albert_base/skim_0.1_balance_20/
 
 mkdir -p ${OUTPUT_DIR}
 
@@ -12,12 +13,13 @@ mkdir -p ${OUTPUT_DIR}
 python -u src/run_squad.py \
   --model_type albert \
   --block_skim \
-  --skim_threshold 0.5 \
+  --actual_skim \
+  --skim_threshold 0.005 \
   --per_gpu_eval_batch_size=20 \
   --model_name_or_path ${EVAL_CKPT_DIR} \
   --cache_name albert-base-v2 \
   --do_eval \
-  --predict_file dev-v1.1.json \
+  --predict_file gold_validation.json \
   --data_dir ${DATA_DIR} \
   --max_seq_length 512 \
   --doc_stride 128 \
