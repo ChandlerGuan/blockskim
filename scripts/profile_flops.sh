af80b4db404a405bd59960d801f0178232f4c5bf
ChandlerGuan <bonboru.guan@gmail.com>
2021-09-07 23:14:58 +0800

    update: hotpotqa experiments

--- scripts/profile_flops.sh
+++ scripts/profile_flops.sh
@@ -1,8 +1,13 @@
 OUTPUT_DIR=model/tmp/eval/prof
-DATA_DIR=datasets/squad
+# DATA_DIR=datasets/squad
+DATA_DIR=/home/yguan/blockskim/datasets/hotpotqa
+# DATA_DIR=datasets/mrqa/NaturalQuestions/
 
-EVAL_CKPT_DIR=model/block_skim/bert_large_wwm_new/skim_1_balance_20_seed_43/
+# EVAL_CKPT_DIR=/home/yguan/blockskim/model/hotpotqa/block_skim_new/skim_0.1_balance_20_seed_42_09-02-11-01/
 # EVAL_CKPT_DIR=model/block_skim/skim_0.1_balance_20/
+# EVAL_CKPT_DIR=model/NewsQA/block_skim/bert_base/skim_0.1_balance_20
+EVAL_CKPT_DIR=/home/yguan/blockskim2/model/NaturalQuestions/block_skim/bert_base/skim_0.1_balance_20
+
 
 mkdir -p ${OUTPUT_DIR}
 
@@ -12,13 +17,13 @@ python -u src/run_squad_profile.py \
   --model_type bert \
   --block_skim \
   --actual_skim \
-  --skim_threshold 0.5 \
-  --per_gpu_eval_batch_size=20 \
+  --skim_threshold 0.01 \
+  --per_gpu_eval_batch_size=1 \
   --model_name_or_path ${EVAL_CKPT_DIR} \
-  --cache_name bert-large-uncased-whole-word-masking \
+  --cache_name bert-base-uncased \
   --do_lower_case \
   --do_eval \
-  --predict_file dev-v1.1.json \
+  --predict_file gold_validation.json  \
   --data_dir ${DATA_DIR} \
   --max_seq_length 512 \
   --doc_stride 128 \
