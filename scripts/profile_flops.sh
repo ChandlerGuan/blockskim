07ffeca9f4119caf7759476712d91c944ce9dd8d
ChandlerGuan <bonboru.guan@gmail.com>
2021-09-07 23:23:54 +0800

    update: flops eval

--- scripts/profile_flops.sh
+++ scripts/profile_flops.sh
@@ -1,9 +1,11 @@
 OUTPUT_DIR=model/tmp/eval/prof
-DATA_DIR=datasets/squad
+# DATA_DIR=datasets/squad
+DATA_DIR=/home/yguan/blockskim/datasets/hotpotqa
 
 # EVAL_CKPT_DIR=model/block_skim/bert_large_wwm_new/skim_1_balance_20_seed_43/
 # EVAL_CKPT_DIR=model/block_skim/skim_0.1_balance_20/
-EVAL_CKPT_DIR=/home/yguan/blockskim2/model/block_skim/distilbert/distill/skim_0.1_balance_20_09-03-12-55
+# EVAL_CKPT_DIR=/home/yguan/blockskim2/model/block_skim/distilbert/distill/skim_0.1_balance_20_09-03-12-55
+EVAL_CKPT_DIR=/home/yguan/blockskim/model/hotpotqa//distilbert/distill/skim_0.1_balance_20_09-06-14-15/
 
 mkdir -p ${OUTPUT_DIR}
 
@@ -11,8 +13,10 @@ mkdir -p ${OUTPUT_DIR}
 python -u src/run_squad_profile.py \
   --fast_eval 100 \
   --model_type distilbert \
-  --skim_threshold 0.01 \
-  --per_gpu_eval_batch_size=20 \
+  --block_skim \
+  --actual_skim \
+  --skim_threshold 0.1 \
+  --per_gpu_eval_batch_size=1 \
   --model_name_or_path ${EVAL_CKPT_DIR} \
   --cache_name distilbert-base-uncased \
   --do_lower_case \
