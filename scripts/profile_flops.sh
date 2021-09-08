237c5b3f5c16310b13278b6153b60928365943c8
ChandlerGuan <bonboru.guan@gmail.com>
2021-09-08 10:58:11 +0800

    update: exp on hotpot

--- scripts/profile_flops.sh
+++ scripts/profile_flops.sh
@@ -2,7 +2,9 @@ OUTPUT_DIR=model/tmp/eval/prof
 DATA_DIR=/home/yguan/blockskim/datasets/hotpotqa
 
 # EVAL_CKPT_DIR=model/block_skim/bert_large_wwm_new/skim_1_balance_20_seed_43/
-EVAL_CKPT_DIR=/home/yguan/blockskim/model/hotpotqa/block_skim_new/skim_0.1_balance_20_seed_42_09-02-11-01/
+# EVAL_CKPT_DIR=/home/yguan/blockskim/model/hotpotqa/block_skim_new/skim_0.1_balance_20_seed_42_09-02-11-01/
+EVAL_CKPT_DIR=model/hotpotqa/bert_large/skim_0.1_balance_20_seed_42/
+
 
 mkdir -p ${OUTPUT_DIR}
 
@@ -12,10 +14,10 @@ python -u src/run_squad_profile.py \
   --model_type bert \
   --block_skim \
   --actual_skim \
-  --skim_threshold 0.1 \
-  --per_gpu_eval_batch_size=20 \
+  --skim_threshold 0.001 \
+  --per_gpu_eval_batch_size=1 \
   --model_name_or_path ${EVAL_CKPT_DIR} \
-  --cache_name bert-base-uncased \
+  --cache_name bert-large-uncased-whole-word-masking \
   --do_lower_case \
   --do_eval \
   --predict_file gold_validation.json \
